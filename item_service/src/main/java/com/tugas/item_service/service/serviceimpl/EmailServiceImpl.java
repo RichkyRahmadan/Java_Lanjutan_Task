40a87d2c63701468b3909ecc34ad8f6816aacfa9
Richky Suaminya Agnes Tachyon <tachrichky@gmail.com>
2026-03-13 19:10:21 +0700

    task 4 v3

--- item_service/src/main/java/com/tugas/item_service/service/serviceimpl/EmailServiceImpl.java
+++ item_service/src/main/java/com/tugas/item_service/service/serviceimpl/EmailServiceImpl.java
@@ -2,27 +2,21 @@ package com.tugas.item_service.service.serviceimpl;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Properties;
-
-import javax.mail.MessagingException;
-import javax.mail.Multipart;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.tugas.item_service.repository.ItemRepository;
 import com.tugas.item_service.service.EmailService;
+
+import jakarta.mail.internet.MimeMessage;
+
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @Service
 public class EmailServiceImpl implements EmailService {
@@ -32,68 +26,33 @@ public class EmailServiceImpl implements EmailService {
     @Autowired
     TemplateEngine templateEngine;
 
+    @Autowired
+    JavaMailSender mailSender;
 
-    @Value("${smtp.host}")
-    private String smtpHost;
-    @Value("${smtp.port}")
-    private String smtpPort;
-    @Value("${email.password}")
-    private String senderPassword;
     @Value("${email.sender}")
     private String senderEmail;
 
     @Override
     public void sendEmailAllItem() throws Exception {
         try {
-            InternetAddress[] senderEmailWithoutName = InternetAddress.parse(senderEmail);
-
-            Properties props = new Properties();
-            props.put("mail.smtp.auth", "true");
-            props.put("mail.smtp.starttls.enable", "true");
-            props.put("mail.smtp.host", smtpHost);
-            props.put("mail.smtp.port", smtpPort);
-
-            Session session = Session.getInstance(props,
-                    new javax.mail.Authenticator() {
-                        protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
-                            return new javax.mail.PasswordAuthentication(senderEmailWithoutName[0].getAddress(),
-                                    senderPassword);
-                        }
-                    });
-
-
             Context context = new Context();
             context.setVariable("judulEmail", "Laporan Stok Barang");
             context.setVariable("listProduk", itemRepository.getAllItem());
 
             String htmlFinal = templateEngine.process("email-template", context);
-            try {
-                LocalDateTime currentDateTime = LocalDateTime.now();
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-                String formattedDateTime = currentDateTime.format(formatter);
+            LocalDateTime currentDateTime = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            String formattedDateTime = currentDateTime.format(formatter);
 
-                javax.mail.Message message = new MimeMessage(session);
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
 
-                message.setFrom(new InternetAddress(senderEmail));
-                message.setRecipients(javax.mail.Message.RecipientType.TO,
-                        InternetAddress.parse(senderEmail));
+            messageHelper.setFrom(senderEmail);
+            messageHelper.setTo(senderEmail);
+            messageHelper.setSubject("Chyone Store Mailing System - " + formattedDateTime + " - Stock Item Currently");
+            messageHelper.setText(htmlFinal, true);
 
-                message.setSubject("Chyone Store Mailing System - " +
-                        formattedDateTime + " - " + "Stock Item Currently");
-
-                MimeBodyPart messageBodyPart = new MimeBodyPart();
-                messageBodyPart.setContent(htmlFinal, "text/html; charset=utf-8");
-
-                Multipart multipart = new MimeMultipart();
-                multipart.addBodyPart(messageBodyPart);
-
-                message.setContent(multipart);
-                Transport.send(message);
-
-            } catch (MessagingException e) {
-                e.printStackTrace();
-                throw new RuntimeException(e);
-            }
+            mailSender.send(message);
 
         } catch (Exception e) {
             e.printStackTrace();
